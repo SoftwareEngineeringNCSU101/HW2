b5eccb707b78a4f94cb5d4e73efc88b404987461
Dinesh12 <dineshkannan010@gmail.com>
2024-09-12 13:23:23 -0400

    Updated code according to static analysis tool

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,23 +1,14 @@
 """
-This module provides a function to create an array of random numbers using the `shuf` command.
+This module generates an array of random integers.
 """
-
-import subprocess  # nosec
+import subprocess
 
 
 def random_array(arr):
-    """
-    Populates the given array with random numbers using the 'shuf' command.
-
-    Args:
-        arr (list): List to populate with random numbers.
-
-    Returns:
-        list: List populated with random numbers.
-    """
-    shuf_command = "/usr/bin/shuf"  # Specify the full path to the shuf command
+    """Generates an array of random integers."""
+    shuffled_num = None
     for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            [shuf_command, "-i1-20", "-n1"], capture_output=True, check=True)  # nosec
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
